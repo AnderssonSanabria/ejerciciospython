7c58a7466ed4a0ab2d423ccaa3a333206d912822
Andersson Sanabria <andersson1493@gmail.com>
2021-05-03 16:43:57 -0500

    ejercicio 10 if

---
+++ ejercicio10.py
@@ -0,0 +1,18 @@
+# Autor:Andersson Sanabria
+# Fecha:03/04/2021
+# Version:1.0
+# Nombre del Proyecto:Ejercicio #10
+
+# ----------------------Ejercicio #10-----------------------------
+
+# Escribir un programa que solicite ingresar cuanto dinero tiene en su cartera,
+# el precio total de los productos que va a comprar y le diga si le alacanza o no
+
+if __name__=='__main__':
+    dinero = float(input('Cuanto dinero tiene en su cartera: '))
+    precio_productos =  float(input('Cual es el precio de los productos: '))
+    
+    if(dinero>=precio_productos):
+        print('El dinero si le alcanza')
+    else:
+        print('El dinero no le alcanza')
