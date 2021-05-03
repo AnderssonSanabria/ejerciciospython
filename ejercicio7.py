bbdd5fc15cea5e5b6092b867b0914ef5709f8803
Andersson Sanabria <andersson1493@gmail.com>
2021-05-03 14:39:29 -0500

    ejercicio 7 operadores logicos

---
+++ ejercicio7.py
@@ -0,0 +1,16 @@
+# Autor:Andersson Sanabria
+# Fecha:28/04/2021
+# Version:1.0
+# Nombre del Proyecto:Ejercicio #7
+
+# ----------------------Ejercicio #7-----------------------------
+# Escribí un programa que solicite al usuario que ingrese cuántos shows musicales 
+# ha visto en el último año y almacene ese número en una variable. A continuación
+#  mostrar en pantalla un valor de verdad (True o False) que indique si el usuario 
+# ha visto más de 3 shows.
+
+if __name__=='__main__':
+    shows = int(input('Ingrese la cantidad de shows musicales que ha visto en el último año: '))
+    masdetres = False
+ 
+print(masdetres >= 3)
